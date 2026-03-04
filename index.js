e5756ca01ee4530bcb98a6485c46de6b298dddaf
idk707 <156857232+idk707@users.noreply.github.com>
2026-03-04 16:02:12 +0000

    Work In Progress

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menu   = document.getElementById('side_menu');
+  const open   = document.getElementById('menu');
+  const close  = document.querySelector('#side_menu .close_button');
+
+  open.addEventListener('click', () => {
+    menu.classList.add('visible');
+  });
+
+  close.addEventListener('click', () => {
+    menu.classList.remove('visible');
+  });
+});
