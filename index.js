9dfcf2ae191037615819e39a5b0e2a41a5413004
idk707 <156857232+idk707@users.noreply.github.com>
2026-03-05 16:54:19 +0000

    Work In Progress

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const title = document.getElementById('title');
-  const menu   = document.getElementById('side_menu');
-  const open   = document.getElementById('menu');
-  const close  = document.querySelector('#side_menu .close_button');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const title = document.getElementById('title');
 
-  open.addEventListener('click', () => {
-    menu.classList.add('visible');
 
-  });
-
-  close.addEventListener('click', () => {
-    menu.classList.remove('visible');
-  });
-
-  requestAnimationFrame(() => {
-    
-  });
-});
+// });
